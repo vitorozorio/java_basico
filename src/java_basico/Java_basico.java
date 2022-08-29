5831a65f8f0edb06997a8cb1175d2e7d7eeb5b8f
zorin375 <vitormanoelbasilio357@gmail.com>
2022-08-28 23:12:57 -0300

    java_basico

---
+++ src/java_basico/Java_basico.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_basico;
+
+/**
+ *
+ * @author zori-dev
+ */
+public class Java_basico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Pessoa p1 = new Pessoa();
+       p1.setNome("fernando");
+       p1.setIdade(33);
+       
+       
+        System.err.println(p1.getNome()+"  -  "+p1.getIdade());
+    }
+    
+}
