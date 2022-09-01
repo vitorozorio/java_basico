c69c32a9e1abe9065b7d4f06c57004ff0fce104f
oliveiradeigor <lrmcpe22@gmail.com>
2022-08-31 21:10:34 -0300

    criando projeto de sintaxe

---
+++ src/sintaxe/Sintaxe.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sintaxe;
+
+/**
+ *
+ * @author igor
+ */
+public class Sintaxe {
+    public static void main(String[] args) {
+        
+        //tipos primitivos
+            //inteiros
+                int numero1 = 10;
+                long numero2 = 10;
+            //ponto flutuantes    
+                float numero3 = 10;
+                double numero4 = 10;
+            //booleana    
+                boolean booleana = true;
+            //char
+                char letra = 'A';
+            //outra forma;
+                char[] letras = new char[10];
+                //ex:
+                letras[0] = 'I'; //tem que ser com aspas assim => '' para funcionar
+                letras[1] = 'G';
+                letras[2] = 'O';
+                letras[3] = 'R';
+        
+    }
+}
